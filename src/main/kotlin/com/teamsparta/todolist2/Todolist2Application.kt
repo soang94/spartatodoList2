cfd01c407ce3a0d48a0da9d8b8900692c7691219
이승상 <soang2533@gmail.com>
2024-01-17 10:55:47 +0900

    init commit

---
+++ src/main/kotlin/com/teamsparta/todolist2/Todolist2Application.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.todolist2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Todolist2Application
+
+fun main(args: Array<String>) {
+    runApplication<Todolist2Application>(*args)
+}
---
+++ src/test/kotlin/com/teamsparta/todolist2/Todolist2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.todolist2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Todolist2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
